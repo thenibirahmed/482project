b256222b50595400090b6a8287d8841f5bbc4722
Nibir Ahmed <nibirahmed9611@gmail.com>
2023-05-28 12:17:44 +0600

    title

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -20,7 +20,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Admin</title>
+	<title>Admin - Travelling Management System</title>
 
 	<link async href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
