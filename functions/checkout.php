467e9281077cff959c933779b2ba2a009cb9f815
Nibir Ahmed <nibirahmed9611@gmail.com>
2023-05-28 09:10:26 +0600

    sms sending

--- functions/checkout.php
+++ functions/checkout.php
@@ -34,12 +34,41 @@ if(isset($_POST['checkout'])){
     $query = mysqli_query($conn, $sql);
 
     if($query){
+
+        $ap_key='175589388382444820230124030139pmShsZYiQC'; 
+        $sender_id='361';
+        $mobile_no=$phone;
+        $message='Your Booking is successful. Have an amazing trip!';
+        $user_email='thenibirahmed@gmail.com';
+        $reponse = sendSMS($ap_key,$sender_id,$mobile_no,$message,$user_email);
+
         $_SESSION['success'] = 'Booking successful';
         header('Location: ../index.php');
     }else{
         $_SESSION['errors'][] = 'Failed to book';
         header('Location: ../checkout.php');
     }
+}
+
+function sendSMS($ap_key,$sender_id,$mobile_no,$message,$user_email){
+	$url = 'https://24bulksms.com/24bulksms/api/api-sms-send';
+	$data = array('api_key' => $ap_key,
+	 'sender_id' => $sender_id,
+	 'message' => $message,
+	 'mobile_no' =>$mobile_no,
+	 'user_email'=> $user_email		
+	 );
+
+	// use key 'http' even if you send the request to https://...
+	$curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);     
+    $output = curl_exec($curl);
+    curl_close($curl);
+	print_r($output); 
 }
 
 
